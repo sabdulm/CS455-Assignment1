fdeb74b948e399cf4cfd685f3b500d79b826515f
Sheikh Abdul Mannan <mannanabdul.ams@gmail.com>
2021-02-24 00:02:11 -0700

    updated runner.sh

--- runner.sh
+++ runner.sh
@@ -10,14 +10,39 @@ REMOTE_JAR_PATH="cs455-assignment1"
 
 # for copying jar file to remote machine
 
-# ssh -l ${USERNAME} ${COLLATOR_HOST} "mkdir ${REMOTE_JAR_PATH}"
-# scp ${JARFILE_PATH} ${USERNAME}@${COLLATOR_HOST}:~/${REMOTE_JAR_PATH}/hw1.jar
+build_jar() {
+    gradle clean
+    gradle build
+}
+
+copy_jar() {
+    ssh -l ${USERNAME} ${COLLATOR_HOST} "mkdir ${REMOTE_JAR_PATH}"
+    scp ${JARFILE_PATH} ${USERNAME}@${COLLATOR_HOST}:~/${REMOTE_JAR_PATH}/hw1.jar
+}
+
+run_jar() {
+    for HOSTNAME in ${NODE_HOSTS} ; do
+        ssh -l ${USERNAME} ${HOSTNAME} java -cp ${REMOTE_JAR_PATH}/hw1.jar cs455.overlay.Main node ${HOSTNAME} ${PORT} ${COLLATOR_HOST} ${PORT} &
+    done
+}
+
+print_usage() {
+    echo "Usage: sh runner.sh -flag\n-b to build jar\n-c to copy jar to remote\n-r to run jar on remote machines"
+}
+
+clean() {
+    gradle clean
+}
+
+getopts 'bcrf' flag;
+    case "${flag}" in 
+    b) build_jar ;;
+    c) copy_jar ;;
+    r) run_jar ;;
+    f) clean ;;
+    *) print_usage
+        exit 1 ;;
+    esac
 
 
-
-# #running messaging nodes
-
-for HOSTNAME in ${NODE_HOSTS} ; do
-    ssh -l ${USERNAME} ${HOSTNAME} java -cp ${REMOTE_JAR_PATH}/hw1.jar cs455.overlay.Main node ${HOSTNAME} ${PORT} ${COLLATOR_HOST} ${PORT} &
-done
 
